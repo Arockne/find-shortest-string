74aa1c8df7d2986b08bb85b22d361ae48109744b
Daniel Calvin <arockne2003@yahoo.com>
2022-02-18 10:28:46 -0700

    commit tree ad66c0c2d98ac31a6f3dbc2872b35d058ff0fca0

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,9 +1,29 @@
+//i
+  //
 function findShortestString(arr) {
-  // type your code here
+
+  // let smallest = arr[0]
+
+  // for (let current of arr) {
+  //   if (current.length < smallest.length) {
+  //     smallest = current
+  //   }
+
+  // }
+  // return smallest;
+
+  return arr.reduce((smallest, current) => {
+    return current.length < smallest.length ? current : smallest;
+  })
 }
 
 if (require.main === module) {
   // add your own tests in here
+  assertEqual('a', findShortestString(['a', 'aaaaaa', 'aaaaaaaaaaa', 'aaaaaaaaaaaaaaa']))
+  console.log('\n\n')
+  assertEqual('in', findShortestString(['damsel', 'in', 'distress', 'in']))
+  console.log('\n\n')
+
   console.log("Expecting: 'a'");
   console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
@@ -17,10 +37,33 @@ if (require.main === module) {
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
+  const strings = [];
+  
+  for (let i = 0; i < 100; i++) {
+
+  }
+
   // BENCHMARK HERE
+  const startTime = Date.now();
+
+  for (let i = 0; i < 1000; ++i) {
+    findShortestString(['flower', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion', 'juniper', 'lily', 'dadelion'])
+  }
+
+  const avgTime = (Date.now() - startTime) / 2000;
+  console.log(avgTime)
 }
 
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
+function assertEqual(a, b) {
+  console.log(`=> ${b}`)
+  if (a === b) {
+    return console.log('passed')
+  }
+  return console.log(`Expected ${a} to equal ${b}`)
+}
